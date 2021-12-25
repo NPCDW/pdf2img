985d67e1d2acc4abcaec7030a509373c0098ec3b
HermitNpc <npc.0.dw@gmail.com>
2021-12-25 21:47:01 +0800

    引入cdn，减小打包体积

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,40 @@
+const cdn = {
+    css: [
+        "//unpkg.zhimg.com/element-ui@2.15.6/lib/theme-chalk/index.css",
+    ],
+    js: [
+        "//unpkg.zhimg.com/vue@2.6.11/dist/vue.min.js",
+        "//unpkg.zhimg.com/element-ui@2.15.6/lib/index.js",
+        "//unpkg.zhimg.com/pdfjs-dist@2.4.456/build/pdf.min.js",
+        "//unpkg.zhimg.com/jszip@3.7.1/dist/jszip.min.js",
+        "//unpkg.zhimg.com/file-saver@2.0.5/dist/FileSaver.min.js",
+    ]
+}
+
 module.exports = {
-    publicPath:"./",  // 可以设置成相对路径，这样所有的资源都会被链接为相对路径，打出来的包可以被部署在任意路径
-    outputDir:"docs",  // 打包时生成的生产环境构建文件的目录
+    publicPath: './',
+    outputDir:"docs",   // 打包时生成的生产环境构建文件的目录
+    productionSourceMap: process.env.NODE_ENV !== 'production',
+    chainWebpack: config => {
+        config.resolve.symlinks(true)
+    },
+    configureWebpack: {
+        externals: {
+            vue: "Vue",
+            "element-ui": "ELEMENT",
+            "pdfjs-dist": "pdfjsLib",
+            "jszip": "JSZip",
+            "file-saver": "saveAs",
+        }
+    },
+    pages: {
+        index: {
+            entry: 'src/main.js',
+            template: 'public/index.html',
+            filename: 'index.html',
+            title: 'PDF转图片',
+            chunks: ['chunk-vendors', 'chunk-common', 'index'],
+            cdn: cdn
+        }
+    }
 }

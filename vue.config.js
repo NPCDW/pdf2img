3a5ac496e6ef69766b3490ef9bb142f940a3c4eb
cyl14082 <cyl14082@cnki.net>
2021-12-27 10:59:38 +0800

    webpack package analysis

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,7 @@ const cdn = {
         "//unpkg.zhimg.com/vue@2.6.11/dist/vue.min.js",
         "//unpkg.zhimg.com/element-ui@2.15.6/lib/index.js",
         "//unpkg.zhimg.com/pdfjs-dist@2.4.456/build/pdf.min.js",
+        "//unpkg.zhimg.com/pdfjs-dist@2.4.456/build/pdf.worker.min.js",
         "//unpkg.zhimg.com/jszip@3.7.1/dist/jszip.min.js",
         "//unpkg.zhimg.com/file-saver@2.0.5/dist/FileSaver.min.js",
     ]
@@ -17,12 +18,18 @@ module.exports = {
     productionSourceMap: process.env.NODE_ENV !== 'production',
     chainWebpack: config => {
         config.resolve.symlinks(true)
+        if (process.env.NODE_ENV === 'production') {
+            config.plugin('webpack-bundle-analyzer')
+                .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
+                .end()
+        }
     },
     configureWebpack: {
         externals: {
             vue: "Vue",
             "element-ui": "ELEMENT",
             "pdfjs-dist": "pdfjsLib",
+            "pdfjs-dist/build/pdf.worker.entry": "pdfjsWorker",
             "jszip": "JSZip",
             "file-saver": "saveAs",
         }
